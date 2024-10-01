7faab63133d785fa330517a89b517941bb58160f
cyrll29 <cvillasenior@gmail.com>
2024-10-01 20:43:55 +0800

    started the progress for navBar

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import './navBar.css'
 
 const navBar = () => {
   return (
-    <div>navBar</div>
+    <div className='nav-bar'>
+      <div className="nav-bar-title">
+        <h1>Harum</h1>
+      </div>
+      <div className="nav-bar-components">
+        
+      </div>
+    </div>
   )
 }
 
