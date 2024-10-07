3a2ee0db01ca68f3a35b177ab40db71598a25c2f
cyrll29 <cvillasenior@gmail.com>
2024-10-07 18:59:25 +0800

    update

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import NavBar from '../../components/navBar/navBar'
 import './home.css';
+import Perfume from '../../assets/perfume.png'
 
 const home = () => {
   return (
@@ -9,7 +10,42 @@ const home = () => {
         <div className="home-body">
           <div className="home-header">
             <div className="home-header-tagline">
-              <h1>Unlock the Essence: <br/>Discover, Explore, and Savor <br/>Every Scent.</h1>
+              <h1>Unlock the Essence &#8208;</h1>
+              <p>Discover, Explore, and Savor <br/>Every Scent.</p>
+            </div>
+            <div className="home-header-image">
+              <img src={Perfume} alt="Perfume" />
+            </div>
+          </div>
+
+          <div className="content-1">
+            <div className="grid-container">
+              <div className="grid one">
+                1
+              </div>
+              <div className="grid one">
+                1
+              </div>
+              <div className="grid two">
+                <div className="grid-2-inside">
+  
+                </div>
+              </div>
+              <div className="grid three">
+                <div className="grid-3-inside">
+  
+                </div>
+              </div>
+              <div className="grid four">
+                <div className="grid-4-inside">
+  
+                </div>
+              </div>
+              <div className="grid five">
+                <div className="grid-5-inside">
+  
+                </div>
+              </div>
             </div>
           </div>
         </div>
